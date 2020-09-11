67593b769a9b76a16f905c42b70fe0a8986f6dab
yxc-9420 <42735456+yxc-9420@users.noreply.github.com>
2020-09-11 14:11:58 +0800

    Update prictice.py

--- prictice.py
+++ prictice.py
@@ -17,6 +17,21 @@ class B(A):
     def __call__(self, *args, **kwargs):
         print('call B')
 
+class Node:
+    def __init__(self,value):
+        self._value = value
+        self._children = []
+    def __repr__(self):
+        return 'Node({!r})'.format(self._value)
+    def add_child(self,node):
+        self._children.append(node)
+    def __iter__(self):
+        return iter(self._children)
+    def depth_first(self):
+        yield self
+        for c in self:
+            yield from c.depth_first()
+            
 
 if __name__ == '__main__':
     b = B()
@@ -25,3 +40,13 @@ if __name__ == '__main__':
     b.__class__ = A
     b.show()
     b()
+    root = Node(0)
+    child1 = Node(1)
+    child2 = Node(2)
+    root.add_child(child1)
+    root.add_child(child2)
+    child1.add_child(Node(3))
+    child1.add_child(Node(4))
+    child2.add_child(Node(5))
+    for ch in root.depth_first():
+        print(ch)
